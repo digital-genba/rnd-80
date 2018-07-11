e0479e32284f15b79df66e04e1cb90422b1809be
Amy O'Rourke <asorourke@agiledigital.com.au>
2018-07-12 09:44:33 +1000

    Use typescript

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+const withTypescript = require('@zeit/next-typescript');
+const fp = require('lodash/fp');
+
+module.exports = fp.compose(withTypescript)({
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
@@ -7,4 +10,4 @@ module.exports = {
 
     return config
   }
-}
+});
