390a806521741d6d493a1533fcbe2b653348a368
Amy O'Rourke <asorourke@agiledigital.com.au>
2018-07-12 14:56:16 +1000

    Minimal draftjs implementation

--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -1,7 +1,27 @@
-export default () => {
-  return (
-    <div>
-      Text
-    </div>
-  );
+import { Editor, EditorState } from 'draft-js';
+import 'draft-js/dist/Draft.css';
+
+class TextEditor extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+
+  _onBoldClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <button onClick={this._onBoldClick}>Bold</button>
+        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+      </div>
+    );
+  }
 }
+
+export default TextEditor;
